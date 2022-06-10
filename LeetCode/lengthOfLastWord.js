f417c5663ca4c7640cf4320a060cd71a2f2c48ee
travis flake <tflake83@gmail.com>
2022-06-10 08:54:32 -0400

    lengthOfLastWord

---
+++ LeetCode/lengthOfLastWord.js
@@ -0,0 +1,9 @@
+/**
+https://leetcode.com/problems/length-of-last-word/
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    return s.trim().split(' ').pop().length
+};
+console.log(lengthOfLastWord("  fly me   to   the moon  "))
