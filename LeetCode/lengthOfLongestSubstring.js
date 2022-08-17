7db044b2fbcbc3dd4562dd828f956802a0d467fe
travis flake <tflake83@gmail.com>
2022-08-17 15:33:22 -0400

    longest_substr

--- LeetCode/lengthOfLongestSubstring.js
+++ LeetCode/lengthOfLongestSubstring.js
@@ -11,60 +11,36 @@
  * @param {string} 
  * @returns {int}
  */
-// var lengthOfLongestSubstring = function (s) {
-//     let solution = 0
 
-//     if (s[0] === ' ' || s.length === 1) {
-//         return 1
-//     } else {
-//         if (s[0] === '') {
-//             return 0
-//         }
-//     }
-
-//     let uniqueArr = [s[0]]
-
-//     for (let i = 1; i < s.length ; i++){
-
-
-//         // for (let j = i + 1; j < s.length + 1; j++){
-
-//             if (!uniqueArr.includes(s[i])) {
-//                     uniqueArr.push(s[i])
-//                     console.log(1,i,uniqueArr)
-
-                    
-                
-//                 if (uniqueArr.length === s.length) {
-//                     solution = uniqueArr.length
-//                     console.log(2,i,uniqueArr)
-
-//                 }
-//             } 
-//             if (solution < uniqueArr.length) {
-//                 console.log(3,i,uniqueArr)
-
-//                 solution = uniqueArr.length
-//             } else {
-//                 uniqueArr = [s[i]]
-//                 console.log(4,i,uniqueArr)
-
-
-//             }
-            
-
-//         // }
+function lengthOfLongestSubstring(s) {
+    let i = 0
+    let count = 0
+    let heldString = ''
     
-//     }
-//     return solution
-// };
-// console.log(lengthOfLongestSubstring("abcabcbb"))//3
-// console.log(lengthOfLongestSubstring("bbbbb"))//1
-// console.log(lengthOfLongestSubstring("pwwkew"))//3
-// console.log(lengthOfLongestSubstring("au"))//2
-// console.log(lengthOfLongestSubstring("aab"))//2
-// console.log(lengthOfLongestSubstring(""))//0
-// console.log(lengthOfLongestSubstring("jbpnbwwd"))//4
+    while (i < s.length) {
+        if (!heldString.includes(s[i])) {
+            heldString += s[i]
+            if (count < heldString.length) {
+                count = heldString.length
+            }
+        } else {
+            let indexOfDuplicate = Array.from(heldString).findIndex((letter) => letter === s[i])
+            heldString = heldString.slice(indexOfDuplicate+1)
+            heldString += s[i]
+        }
+        i++
+    }
+    return count
+    
+    
+}
+console.log(lengthOfLongestSubstring("ohvhjdml"))//6
+console.log(lengthOfLongestSubstring("bbbbb"))//1
+ console.log(lengthOfLongestSubstring("pwwkew"))//3
+console.log(lengthOfLongestSubstring("au"))//2
+console.log(lengthOfLongestSubstring("aab"))//2
+console.log(lengthOfLongestSubstring("dvdf"))//3
+console.log(lengthOfLongestSubstring("jbpnbwwd"))//4
 
 
 //push onto newArr until i reaches numRows
